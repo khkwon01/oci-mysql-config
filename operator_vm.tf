c2ca76d643de16845802f20c6a2d9886b5d2c205
khkwon01 <khkwon01@gmail.com>
2023-08-16 15:45:41 +0900

    Create operator_vm.tf

---
+++ operator_vm.tf
@@ -0,0 +1,34 @@
+data "oci_core_images" "images_for_shape" {
+    compartment_id = var.compartment_ocid
+    operating_system = var.operator_os
+    operating_system_version = var.operator_os_version
+    shape = var.operator_shape
+    sort_by = "TIMECREATED"
+    sort_order = "DESC"
+}
+
+resource "oci_core_instance" "operator" {
+
+  availability_domain = data.oci_identity_availability_domain.AD-1.name
+  compartment_id      = var.compartment_ocid
+  display_name        = var.operator_name
+  shape               = var.operator_shape
+
+  shape_config {
+    memory_in_gbs = var.operator_shape_memory
+    ocpus = var.operator_shape_ocpus
+  }
+
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.operator-subnet-regional.id
+  }
+
+  metadata = {
+    ssh_authorized_keys = var.operator_ssh_public_key
+  }
+
+  source_details {
+    source_id   = data.oci_core_images.images_for_shape.images[0].id
+    source_type = "image"
+  }
+}
