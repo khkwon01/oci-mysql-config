88f779e15da79b38d99e51e9f1a64f397435fc10
khkwon01 <khkwon01@gmail.com>
2023-08-16 13:10:31 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -21,60 +21,11 @@ variable "existing_vcn_ocid" {
   default     = ""
 }
 
-variable "existing_public_subnet_ocid" {
-  description = "OCID of an existing public subnet to use"
-  default     = ""
-}
-
 variable "existing_private_subnet_ocid" {
   description = "OCID of an existing private subnet to use"
   default     = ""
 }
 
-variable "existing_internet_gateway_ocid" {
-  description = "OCID of an existing internet gateway to use"
-  default     = ""
-}
-
-variable "existing_nat_gateway_ocid" {
-  description = "OCID of an existing NAT gateway to use"
-  default     = ""
-}
-
-variable "existing_public_route_table_ocid" {
-  description = "OCID of an existing public route table to use"
-  default     = ""
-}
-
-variable "private_key_path" {
-  description = "The private key path to pem. DO NOT FILL WHEN USING RESOURCE MANAGER STACK! "
-  default     = ""
-}
-
-variable "existing_private_route_table_ocid" {
-  description = "OCID of an existing private route table to use"
-  default     = ""
-}
-
-variable "existing_public_security_list_ocid" {
-  description = "OCID of an existing public security list (ssh) to use"
-  default     = ""
-}
-
-variable "existing_public_security_list_http_ocid" {
-  description = "OCID of an existing security list allowing https and https to use"
-  default     = ""
-}
-
-variable "existing_private_security_list_ocid" {
-  description = "OCID of an existing private security list allowing MySQL access to use"
-  default     = ""
-}
-
-variable "existing_mds_instance_ocid" {
-  description = "OCID of an existing MDS instance to use"
-  default     = ""
-}
 
 variable "vcn" {
   description = "VCN Name"
@@ -85,17 +36,6 @@ variable "vcn_cidr" {
   description = "VCN's CIDR IP Block"
   default     = "10.5.0.0/16"
 }
-
-variable "fingerprint" {
-  description = "Key Fingerprint"
-  default     = ""
-}
-
-variable "dns_label" {
-  description = "Allows assignment of DNS hostname when launching an Instance. "
-  default     = ""
-}
-
 
 variable "admin_password" {
   description = "Password for the admin user for MySQL Database Service"
