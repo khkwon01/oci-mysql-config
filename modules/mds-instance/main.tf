262d46bd1d7feab4b94c0b1a538ffe85863886d6
khkwon01 <khkwon01@gmail.com>
2023-08-16 13:29:50 +0900

    Update main.tf

--- modules/mds-instance/main.tf
+++ modules/mds-instance/main.tf
@@ -1,8 +1,3 @@
-
-locals {
-    db_system_id = var.existing_mds_instance_id ==  "" ? oci_mysql_mysql_db_system.MDSinstance[0].id : var.existing_mds_instance_id
-}
-
 resource "oci_mysql_mysql_db_system" "MDSinstance" {
     admin_password = var.admin_password
     admin_username = var.admin_username
@@ -13,8 +8,6 @@ resource "oci_mysql_mysql_db_system" "MDSinstance" {
     subnet_id = var.subnet_id
     data_storage_size_in_gb = var.mysql_data_storage_in_gb
     display_name = var.display_name
-
-    count = var.existing_mds_instance_id == "" ? 1 : 0
 
     is_highly_available = var.deploy_ha
 }
